33edd865dd78c20d01e29671797412c78b06350a
Pascal Hartig <phartig@rdrei.net>
2014-02-02 03:37:50 +0000

    Better design for sorting

--- app/scripts/components/app.jsx
+++ app/scripts/components/app.jsx
@@ -14,8 +14,21 @@ define(function (require) {
         getInitialState: function () {
             return {
                 dragged: -1,
-                placeholder: -1
+                placeholder: -1,
+                // I initially just relied on the upstream component reacting to
+                // onSort and updating the array before I realized that this is
+                // a horrible idea because it breaks the isolation of the
+                // component. You want the list to be sortable no matter what
+                // the outer component does.
+                items: this.props.items
             };
+        },
+
+        componentWillReceiveProps: function (props) {
+            // Props take precedence state for items.
+            this.setState({
+                items: props.items
+            });
         },
 
         render: function () {
@@ -25,7 +38,7 @@ define(function (require) {
         },
 
         renderItems: function () {
-            return this.props.items.map(function (item, i) {
+            return this.state.items.map(function (item, i) {
                 var key = item.key;
 
                 if (!key) {
@@ -61,6 +74,7 @@ define(function (require) {
                 placeholder: -1
             });
 
+            this.props.onSort(this.state.items);
         },
 
         handleDragOver: function (i, e) {
@@ -68,19 +82,18 @@ define(function (require) {
             e.preventDefault();
 
             var dragged = this.state.dragged;
-            var items = this.swapItems(dragged, i);
+            var items = this.swapItems(dragged, i, true);
 
             this.setState({
                 placeholder: i,
                 dragged: i,
+                items: items
             });
-
-            this.props.onSort(items);
         },
 
-        swapItems: function (a, b) {
+        swapItems: function (a, b, inplace) {
             // Could copy, but doesn't matter.
-            var items = this.props.items.slice(0);
+            var items = inplace ? this.state.items : this.state.items.slice(0);
 
             if (a < 0 || b < 0) {
                 console.warn('invalid state');
