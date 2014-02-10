2b4cdab21e2d4b28e9029032437ccaedb086096a
Pascal Hartig <passy@twitter.com>
2014-02-10 20:18:34 +0000

    Animations, but not quite there yet

--- app/scripts/components/global_progress_bar.jsx
+++ app/scripts/components/global_progress_bar.jsx
@@ -3,6 +3,7 @@ define(function (require) {
     'use strict';
 
     var React = require('react');
+    var TransitionGroup = React.addons.TransitionGroup;
     var mediator = require('mediator');
     var WithMediator = require('components/with_mediator');
 
@@ -19,21 +20,46 @@ define(function (require) {
         componentDidMount: function () {
             this.on('uiShowProgressGlobal', function (data) {
                 this.replaceState({
-                    value: data.value
+                    value: 0,
+                    visible: true
                 });
+
+                setTimeout(function () {
+                    this.setState({
+                        value: data.value
+                    });
+                }.bind(this), 100);
             }.bind(this));
 
             this.on('uiHideProgressGlobal', function () {
-                this.replaceState({
-                    value: 0
+                this.setState({
+                    value: 100
                 });
+                setTimeout(function () {
+                    this.replaceState({
+                        value: 0,
+                        visibile: false
+                    });
+                }.bind(this), 1000);
             }.bind(this));
         },
 
+        renderProgressBar: function () {
+            if (this.state.visible) {
+                console.log('visible');
+                return <div className="global-progress"
+                    data-progress={this.state.value}>
+                </div>;
+            }
+
+            return <span />;
+        },
+
         render: function () {
-            return <div className="global-progress"
-                data-progress={this.state.value}>
-            </div>;
+
+            return <TransitionGroup transitionName="fade">
+                {this.renderProgressBar()}
+            </TransitionGroup>;
         }
     });
 });
