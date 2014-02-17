ccab181d104cbbe361cde048d58649595f292d19
Pascal Hartig <passy@twitter.com>
2014-02-17 19:51:25 +0000

    Add rosetta icons

--- app/scripts/components/tweet.jsx
+++ app/scripts/components/tweet.jsx
@@ -8,15 +8,27 @@ define(function (require) {
 
             /*jshint camelcase:false */
             return <article className="tweet">
-                <header className="tweet__header" style={{display: 'none'}}>
-                    <time className="tweet__time" datetime={this.props.tweet.created_at}>
-                        {this.props.tweet.created_at}
+                <header className="tweet__header">
+                    <time className="tweet__time" dateTime={tweet.created_at}>
+                        {tweet.created_at}
                     </time>
-                    <a target="_blank" href={'https://twitter.com/' + tweet.user.screen_name}>
-                        {tweet.user.name}
+                    <a target="_blank" href={'https://twitter.com/' + tweet.user.screen_name} rel="author">
+                        <div className="obj__left">
+                            <img className="tweet__avatar" src={tweet.user.profile_image_url_https} alt={'@' + tweet.user.screen_name + ' Avatar'} />
+                        </div>
+                        <b className="fullname">{tweet.user.name}</b>
+                        <span className="screenname">
+                            <span className="at">@</span>
+                            {tweet.user.screen_name}
+                        </span>
                     </a>
                 </header>
-                <blockquote>{this.props.tweet.text}</blockquote>
+                <blockquote>{tweet.text}</blockquote>
+                <footer className="tweet__actions">
+                    <a href="#">
+                        <i className="icon icon--favorite" title="Favorite" />
+                    </a>
+                </footer>
             </article>;
         }
     });
