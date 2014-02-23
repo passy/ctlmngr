b8560cf9bc53754a91d0d6399aaee5117b83840c
Pascal Hartig <phartig@rdrei.net>
2014-02-23 12:10:58 +0000

    Fix web-font loading

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,8 @@ module.exports = {
             {test: /\.png$/, loader: 'url-loader?limit=100000&mimetype=image/png'},
             {test: /\.jpg$/, loader: 'file-loader'},
             {test: /\.woff$/, loader: 'url-loader?limit=100000&mimetype=application/font-woff'},
+            {test: /\.ttf$/, loader: 'file-loader?mimetype=application/x-font-ttf'},
+            {test: /\.eot$/, loader: 'file-loader?mimetype=application/vnd.ms-fontobject'},
             {test: /\.svg$/, loader: 'url-loader?limit=100000&mimetype=image/svg+xml'},
         ]
     }
