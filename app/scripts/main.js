1fdcab86abbf53764036e384cceb5ab64c7d59a4
Pascal Hartig <phartig@rdrei.net>
2014-02-23 12:02:02 +0000

    Switch to my version of flatly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 require('bootstrap-flatly/flatly.css');
 
 require('../styles/main.css');
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,50 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config');
 
+gulp.task('build', function (cb) {
+    var config = Object.create(webpackConfig);
+
+    config.plugins = (config.plugins || []).concat(
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+
+    webpack(config, function (err, stats) {
+        if (err) {
+            throw new gulpUtil.PluginError('webpack-build', err);
+        }
+
+        gulpUtil.log('[webpack-build]', stats.toString({
+            colors: true
+        }));
+
+        cb();
+    });
+});
+
+gulp.task('build-dev', function (cb) {
+    var config = Object.create(webpackConfig);
+
+    config.devtool = 'eval';
+    config.debug = true;
+
+    webpack(config, function (err, stats) {
+        if (err) {
+            throw new gulpUtil.PluginError('webpack-build-dev', err);
+        }
+
+        gulpUtil.log('[webpack-build-dev]', stats.toString({
+            colors: true
+        }));
+
+        cb();
+    });
+});
 
 gulp.task('serve', function () {
     var config = Object.create(webpackConfig);
@@ -26,3 +70,5 @@ gulp.task('serve', function () {
                      'http://localhost:9000/webpack-dev-server/index.html');
     });
 });
+
+gulp.task('default', ['build']);
