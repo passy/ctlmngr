031569d4cfd6041298bbcc9944400192af048153
Pascal Hartig <passy@twitter.com>
2014-03-04 23:03:15 +0000

    Add HSTS and some other security headers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const connect = require('connect');
 const path = require('path');
+const hood = require('hood');
 
 const STATIC_OPTIONS = {
     root: path.join(process.cwd(), 'app'),
@@ -13,6 +14,11 @@ const STATIC_OPTIONS = {
 
 const app = connect()
     .use(connect.static(STATIC_OPTIONS.root, STATIC_OPTIONS))
-    .use(connect.compress());
+    .use(connect.compress())
+    .use(hood.hsts({
+        maxAge: 31536000
+    }))
+    .use(hood.nosniff())
+    .use(hood.xframe());
 
 app.listen(process.env.PORT || 5000);
