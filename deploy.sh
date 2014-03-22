7b67a904a6bb7c89c92c77fc4a1710b865a61b24
Pascal Hartig <passy@twitter.com>
2014-03-22 17:43:04 +0000

    Deployment fix

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,8 @@ git stash
 git branch -D build || true
 git checkout -b build master
 grunt
-rm -f .gitignore
-git add -A dist/
+git add -Af dist/
 git commit -m "deploy"
-git push heroku `git subtree split --prefix dist build`:master --force
+git push heroku build:master --force
 git checkout master
 git stash pop
