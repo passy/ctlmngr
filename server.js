03f4cc342a287ca19ec4113cbb141624b96e4c7d
Pascal Hartig <passy@twitter.com>
2014-03-22 18:13:18 +0000

    Use st for serving

--- server.js
+++ server.js
@@ -3,13 +3,17 @@
 'use strict';
 
 const connect = require('connect');
+var st = require('st');
 const path = require('path');
 const hood = require('hood');
 
 const STATIC_OPTIONS = {
-    root: path.join(process.cwd(), 'dist'),
-    maxAge: 86400000,
-    redirect: false
+    path: path.join(process.cwd(), 'dist'),
+    index: 'index.html',
+    cache: {
+        max: 1024 * 1024 * 64,
+        maxAge: 1000 * 60 * 24 * 7
+    }
 };
 
 const app = connect()
@@ -18,7 +22,6 @@ const app = connect()
     }))
     .use(hood.nosniff())
     .use(hood.xframe())
-    .use(connect.static(STATIC_OPTIONS.root, STATIC_OPTIONS))
-    .use(connect.compress());
+    .use(st(STATIC_OPTIONS));
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 9000);
